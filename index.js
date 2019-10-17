f944806bd59d81a3db4c4c2b00a3bba99712ff95
saeed.m <saeed.moghadamzadeh@scicom.com.my>
2019-10-17 17:51:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,190 @@
+var fs = require('fs');
+var inquirer = require('inquirer');
+var ncp = require('ncp').ncp;
+
+inquirer
+    .prompt([
+        {
+            type: 'text',
+            message: 'Name',
+            name: 'name',
+        },
+        {
+            type: 'text',
+            message: 'Author',
+            name: 'author',
+        },
+        {
+            type: 'text',
+            message: 'Description',
+            name: 'desc',
+        },
+        {
+            type: 'text',
+            message: 'version',
+            name: 'version',
+            default: "0.1",
+        },
+        {
+            type: 'checkbox',
+            message: 'Select permissions',
+            name: 'permissions',
+            choices: [
+                {
+                    name: "activeTab",
+                },
+                {
+                    name: "alarms",
+                },
+                {
+                    name: "background",
+                },
+                {
+                    name: "bookmarks",
+                },
+                {
+                    name: "browserSettings",
+                },
+                {
+                    name: "browsingData",
+                },
+                {
+                    name: "clipboardRead",
+                },
+                {
+                    name: "clipboardWrite",
+                },
+                {
+                    name: "contentSettings",
+                },
+                {
+                    name: "contextMenus",
+                },
+                {
+                    name: "contextualIdentities",
+                },
+                {
+                    name: "cookies",
+                },
+                {
+                    name: "debugger",
+                },
+                {
+                    name: "dns",
+                },
+                {
+                    name: "downloads",
+                },
+                {
+                    name: "downloads.open",
+                },
+                {
+                    name: "find",
+                },
+                {
+                    name: "geolocation",
+                },
+                {
+                    name: "history",
+                },
+                {
+                    name: "identity",
+                },
+                {
+                    name: "idle",
+                },
+                {
+                    name: "management",
+                },
+                {
+                    name: "menus",
+                },
+                {
+                    name: "menus.overrideContext",
+                },
+                {
+                    name: "nativeMessaging",
+                },
+                {
+                    name: "notifications",
+                },
+                {
+                    name: "pageCapture",
+                },
+                {
+                    name: "pkcs11",
+                },
+                {
+                    name: "privacy",
+                },
+                {
+                    name: "proxy",
+                },
+                {
+                    name: "search",
+                },
+                {
+                    name: "sessions",
+                },
+                {
+                    name: "storage",
+                },
+                {
+                    name: "tabHide",
+                },
+                {
+                    name: "tabs",
+                },
+                {
+                    name: "theme",
+                },
+                {
+                    name: "topSites",
+                },
+                {
+                    name: "unlimitedStorage",
+                },
+                {
+                    name: "webNavigation",
+                },
+                {
+                    name: "webRequest",
+                },
+                {
+                    name: "webRequestBlocking",
+                },
+
+            ]
+        }
+    ])
+    .then(answers => {
+        var manifest = {
+            "manifest_version": 2,
+            author: answers.author,
+            name: answers.name,
+            description: answers.desc,
+            version: "0.1",
+            permissions: answers.permissions,
+            icons: {
+                "48": "assets/images/icons/icon-48.png",
+                "96": "assets/images/icons/icon-96.png"
+            }
+        };
+
+        fs.appendFile('./manifest.json', JSON.stringify(manifest), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          }); 
+
+        ncp.limit = 16;
+        var path = require('path');
+        var appDir = path.dirname(require.main.filename)+"/template";
+        console.log(appDir);
+        ncp(appDir, "./", function (err) {
+         if (err) {
+           return console.error(err);
+         }
+         console.log('done!');
+        });
+
+    });
